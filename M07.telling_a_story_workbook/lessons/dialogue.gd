6e6938f9160f9e38458cb6c2f0f2b4b4aba93467
jngdotcom <jaguo4@s.sfusd.edu>
2025-10-31 15:41:28 -0700

    m7l7

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -1,40 +1,9 @@
 extends Control
 
-@onready var rich_text_label: RichTextLabel = %RichTextLabel
-@onready var next_button: Button = %NextButton
-@onready var audio_stream_player: AudioStreamPlayer2D = %AudioStreamPlayer
-
-var dialogue_items: Array[String] = [
-	"onefish twofish",
-	"red fish",
-	"bluefish",
-]
-
-var current_item_index := 0
-
-func show_text() -> void:
-	var current_item := dialogue_items[current_item_index]
-	rich_text_label.text = current_item
-	rich_text_label.visible_ratio = 0.0
-	
-	var tween := create_tween()
-	var text_appearing_duration := 1.2
-	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
-	
-	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
-	var sound_start_position := randf() * sound_max_offset
-	audio_stream_player.play (sound_start_position)
-	
-	tween.finished.connect(audio_stream_player.stop)
-	
-func _ready() -> void:
-	show_text()
-	next_button.pressed.connect(advance)
-	
-func advance() -> void:
-	current_item_index += 1
-	if current_item_index == dialogue_items.size():
-		get_tree().quit()
-	else:
-		show_text()
-		
+@onready var body: TextureRect = %body
+@onready var expression: TextureRect = %expression
+@onready var button_sophia: Button = %ButtonSophia
+@onready var button_pink: Button = %ButtonPink
+@onready var button_regular: Button = %ButtonRegular
+@onready var button_sad: Button = %ButtonSad
+@onready var button_happy: Button = %ButtonHappy
