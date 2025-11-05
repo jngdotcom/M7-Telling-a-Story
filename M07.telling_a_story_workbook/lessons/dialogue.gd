ef1eb5a8d1df1b8b0a6ff7faa1b00efb5682d545
jngdotcom <jaguo4@s.sfusd.edu>
2025-11-05 13:48:54 -0800

    m7l10

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -1,58 +1,72 @@
 extends Control
 
-@onready var body: TextureRect = %body
-@onready var expression: TextureRect = %expression
-
-@onready var row_bodies: HBoxContainer = %RowBodies
-@onready var row_expressions: HBoxContainer = %RowExpressions
-
-
-var bodies := {
-	"sophia": preload("res://assets/sophia.png"),
-	"pink": preload("res://assets/pink.png")
-}
-
 var expressions := {
 	"happy": preload("res://assets/emotion_happy.png"),
 	"regular": preload("res://assets/emotion_regular.png"),
 	"sad": preload("res://assets/emotion_sad.png")
 }
 
+var dialogue_items: Array[Dictionary] = [
+	{
+	"expressions":expression["regular"],
+	"text": "onefish"
+	},
+	
+	{
+	"expressions": expression["regular"],
+	"text": "two fish"
+	},
+	
+	{
+	"expressions":expression["regular"],
+	"text": "redfish blue"
+	},
+	
+	{
+	"expressions": expression["regular"],
+	"text": "fish"
+	}
+]
+
+var current_item_index := 0
+
+@onready var rich_text_label: RichTextLabel = %RichTextLabel
+@onready var next_button: Button = %NextButton
+@onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
+
+@onready var body: TextureRect = %body
+@onready var expression: TextureRect = %expression
+
+
+
+
 
 func _ready() -> void:
-	create_buttons()
+	show_text()
+	next_button.pressed.connect(advance)
 
+func show_text() -> void:
+	var current_item := dialogue_items[current_item_index]
+	rich_text_label.text = current_item["text"]
+	expression.texture = current_item["expression"]
+	
 
-func create_button_pink() -> void:
-	var button := Button.new()
-	row_bodies.add_child(button)
+	rich_text_label.visible_ratio = 0.0
+	var tween := create_tween()
+	var text_appearing_duration := 1.2
+	tween.tween_property(rich_text_label, "visible_ratio", 1.2, text_appearing_duration)
+	
+	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
+	var sound_start_position := randf() * sound_max_offset
+	audio_stream_player.play(sound_start_position)
+	tween.finished.connect(audio_stream_player.stop)
 
-	var key := "pink"
-	button.text = key.capitalize()
-	button.pressed.connect(func() -> void:
-		body.texture = bodies[key]
-	)
-	
-	
-func create_buttons() -> void:
-	for current_body: String in bodies:
-		var button := Button.new()
-		row_bodies.add_child(button)
-		button.text = current_body.capitalize()
-		button.pressed.connect(func() -> void:
-			body.texture = bodies[current_body]
-	)
-			
-	for current_expression: String in expressions:
-		var button := Button.new()
-		row_expressions.add_child(button)
-		button.text = current_expression.capitalize()
-		button.pressed.connect(func() -> void:
-			expression.texture = expressions[current_expression]
-	)
-		
-	
-		
+func advance() -> void:
+	current_item_index += 1
+	if current_item_index == dialogue_items.size():
+		get_tree().quit()
+	else:
+		show_text()
 
 
 
